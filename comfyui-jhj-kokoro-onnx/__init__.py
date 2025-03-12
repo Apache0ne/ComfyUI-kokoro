1fac07388e7847c2700921a95a626c9fd1e66e4d
Koston Speakman <xcoolcat502@gmail.com>
2025-03-12 13:45:50 -0400

    Delete comfyui-jhj-kokoro-onnx directory

--- comfyui-jhj-kokoro-onnx/__init__.py
+++
@@ -1,9 +0,0 @@
-from .nodes import *
-
-
-NODE_CLASS_MAPPINGS = {
-    "(Down)Load Kokoro Model": KokoroModelLoader,
-    "Kokoro Audio Generator": KokoroAudioGenerator,
-}
-
-__all__ = ['NODE_CLASS_MAPPINGS']
